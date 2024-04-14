6c86a4e4e4d20dfc29a8a9d4b4a4239d62e255cd
joshika7 <160121733075joshika@gmail.com>
2024-04-14 22:02:45 +0530

    first commit

--- water_jug.py
+++ water_jug.py
@@ -1,50 +1,66 @@
-class Node:
-    def __init__(self, state, parent):
-        self.state = state
-        self.parent = parent
+from collections import deque
 
-    def get_child_nodes(self, capacities):
-        a, b = self.state
-        max_a, max_b = capacities
-        children = []
-        children.append(Node((max_a, b), self))
-        children.append(Node((a, max_b), self))
-        children.append(Node((0, b), self))
-        children.append(Node((a, 0), self))
-        if a + b >= max_b:
-            children.append(Node((a - (max_b - b), max_b), self))
-        else:
-            children.append(Node((0, a + b), self))
-        if a + b >= max_a:
-            children.append(Node((max_a, b - (max_a - a)), self))
-        else:
-            children.append(Node((a + b, 0), self))
-        return children
+def pour(state, jug1, jug2):
+   
+    amt = min(state[jug1], (jug_caps[jug2] - state[jug2]))
+    new_state = list(state)
+    new_state[jug1] -= amt
+    new_state[jug2] += amt
+    return tuple(new_state)
 
-def dfs(start_state, goal_state, capacities):
-    start_node = Node(start_state, None)
-    visited = set()
-    stack = [start_node]
-    while stack:
-        node = stack.pop()
-        if node.state == goal_state:
-            path = []
-            while node.parent:
-                path.append(node.state)
-                node = node.parent
-            path.append(start_state)
-            path.reverse()
-            return path
-        if node.state not in visited:
-            visited.add(node.state)
-            for child in node.get_child_nodes(capacities):
-                stack.append(child)
+def get_successors(state):
+    
+    successors = []
+    for jug1, jug2 in [(0, 1), (1, 0)]:
+        new_state = pour(state, jug1, jug2)
+        if new_state != state:
+            successors.append(new_state)
+    for jug in [0, 1]:
+        new_state = list(state)
+        new_state[jug] = jug_caps[jug]
+        successors.append(tuple(new_state))
+    for jug in [0, 1]:
+        new_state = list(state)
+        new_state[jug] = 0
+        successors.append(tuple(new_state))
+    return successors
+
+def heuristic(state, goal):
+
+    return sum(abs(state[i] - goal[i]) for i in range(len(state)))
+
+def a_star(start, goal):
+   
+    open_list = [(heuristic(start, goal), start)]
+    print(open_list)
+    closed_list = set()
+    parent = {start: None}
+    while open_list:
+        _, curr_state = open_list.pop(0)
+        if curr_state == goal:
+            path = deque()
+            state = curr_state
+            while state is not None:
+                path.appendleft(state)
+                state = parent[state]
+            return list(path)
+        closed_list.add(curr_state)
+        for succ_state in get_successors(curr_state):
+            if succ_state not in closed_list:
+                succ_cost = heuristic(succ_state, goal)
+                open_list.append((succ_cost, succ_state))
+                open_list.sort()
+                parent[succ_state] = curr_state
     return None
 
+jug_caps = (4, 3)
 start_state = (0, 0)
-a, b = map(int, input("Enter the capacities of jugs: ").split())
-c, d = map(int, input("Enter the capacities of goal state: ").split())
-goal_state = (c, d)
-capacities = (a, b)
-path = dfs(start_state, goal_state, capacities)
-print(path)
+goal_state = (2, 0)
+
+solution = a_star(start_state, goal_state)
+if solution:
+    print("Solution:")
+    for state in solution:
+        print(state)
+else:
+    print("No solution exists.")
